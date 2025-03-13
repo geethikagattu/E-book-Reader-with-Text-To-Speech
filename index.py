cc410ab671c39900dd3976c4357a28d9ae06d679
Geethika <geethikagt30@gmail.com>
2025-03-13 18:54:58 +0530

    Done

--- index.py
+++ index.py
@@ -5,14 +5,6 @@ import speech_recognition as sr
 from gtts import gTTS
 import fitz
 
-# def speak(text):
-#     tts = gTTS(text = text , lang = "te")
-#     filename = "voice.mp3"
-#     tts.save(filename)
-#     playsound.playsound(filename)
-
-# speak("హలో, మీరు ఎలా ఉన్నారు?")
-
 doc = fitz.open("/Users/geethikagattu/Desktop/python project/sample.pdf")
 print(doc.page_count)
 
