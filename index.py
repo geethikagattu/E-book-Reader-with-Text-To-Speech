ba98423ff02c5d9917346ce1f327d5d250ab15ff
Geethika <geethikagt30@gmail.com>
2025-03-14 19:36:54 +0530

    New changes

--- index.py
+++ index.py
@@ -1,18 +1,17 @@
 import os
 import time
 import playsound
-import speech_recognition as sr  
 from gtts import gTTS
 import fitz
 
-doc = fitz.open("/Users/geethikagattu/Desktop/python project/sample.pdf")
+doc = fitz.open("/Users/geethikagattu/Desktop/python project/A-Good-Girls-Guide-Series.pdf")
 print(doc.page_count)
 
-page = doc.load_page(0)
-print(page.get_text())
-read = page.get_text()
-
-tts = gTTS(text = read , lang = "en")
-filename = "voice.mp3"
-tts.save(filename)
-playsound.playsound(filename)
+for i in range(13 ,doc.page_count): 
+ page = doc.load_page(i)
+ print(page.get_text())
+ read = page.get_text()
+ tts = gTTS(text = read , lang = "en")
+ filename = "voice.mp3"
+ tts.save(filename)
+ playsound.playsound(filename)
